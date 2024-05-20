fb430ce3d23625d77740da49af5e3466b9107659
Nishireisan <andrey240300@gmail.com>
2024-05-20 18:26:23 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kol_dz = int(input())
+kol_chas = float(input())
+nazvanie = str(input())
+VrNaDz = kol_chas/kol_dz
+print('Курс:', nazvanie, ', всего задач:', kol_dz, ', затрачено часов:', kol_chas, ', среднее время выполнения:', VrNaDz, '.')
